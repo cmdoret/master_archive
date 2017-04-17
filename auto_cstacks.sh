75e52dabeb874307408c1450f0b59dcbbc2e29f5
cmdoret <cyril.mattheydoret@gmail.com>
2017-04-18 01:10:24 +0200

    working on cstacks script, getting core dumped error

--- auto_cstacks.sh
+++ auto_cstacks.sh
@@ -1,7 +1,7 @@
- 
+#!/bin/bash 
 
 for M in 1 2 3 4;
 do
-    sed -i 's/MM=[0-9]+/MM=$M/g' ./src/stacks_pipeline/sub_cstacks.sh
-    bsub <./src/stacks_pipeline/sub_cstacks.sh
+    sed -i "s/\(MM=\)[0-9]*/\1$M/g" src/stacks_pipeline/sub_cstacks.sh;
+    bsub <./src/stacks_pipeline/sub_cstacks.sh;
 done;
--- src/stacks_pipeline/sub_cstacks.sh
+++ src/stacks_pipeline/sub_cstacks.sh
@@ -1,26 +1,29 @@
-!/bin/bash
+#!/bin/bash
 
 #BSUB -o %J_STDOUT.log
 #BSUB -e %J_STDERR.log
 #BSUB -J Cstacks_${j}
 #BSUB -n 3
-#BSUB -M 2000000
-#BSUB -q priority
+#BSUB -R "span[ptile=3]"
+#BSUB -R "rusage[mem=30000]"
+#BSUB -M 30000000
+#BSUB -q normal
 
 ## Simple script for submitting pstacks jobs
 
 wd=/scratch/beegfs/monthly/cmatthey/data ## the directory containing the pstacks files
 MM=1
 
-mkdir -p $wd/data/cstacks/mm-$MM
+rm -r $wd/data/cstacks/mm-$MM;
+mkdir -p $wd/data/cstacks/mm-$MM;
 
 
 samp=""
 for i in $wd/pstacks/covmin-4/*allel*;
 do
-    sam+="-s ${i%%.*}* "
+    samp+="-s ${i%%.alleles*} "
 done;
 
-module add UHTS/Analysis/stacks/1.30
+module add UHTS/Analysis/stacks/1.30;
 
-cstacks -o $wd/cstacks/mm-$MM -n $MM -p 3 $sam
+cstacks -o $wd/cstacks/mm-$MM -n $MM -p 3 $samp;
