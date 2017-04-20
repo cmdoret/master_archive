4fa543d43ccb801a339a97ff5e19697279c3ea96
cmdoret <cyril.mattheydoret@gmail.com>
2017-04-21 00:22:24 +0200

    added cstacks output to lab book

---
+++ src/stacks_pipeline/parse_cstacks.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+# This script parses the output of cstacks into a table to determine the best value for minimum coverage.
+# Cyril Matthey-Doret
+# 11.04.2017
+
+
+cdir=/scratch/beegfs/monthly/cmatthey/data/cstacks
+echo 'mismatch,mean_loci,mean_alleles' > cstats.csv
+for d in $cdir/*;
+do
+    f=$d/*allel*
+
+    # Number of loci
+    L=$(zcat $f | cut  -f3 | uniq | wc -l);
+    # Total number of alleles
+    A=$(zcat $f | wc -l);
+
+    echo ${d##*mm-}','$L','$A >> cstats.csv;
+done
+
+cp cstats.csv ../../reports/lab_book/
