3eea8aa33f4fe67f8dc11f23e46aa2079bc1a3b0
cmdoret <cyril.mattheydoret@gmail.com>
2017-04-25 18:21:30 +0200

    updated lab book with detailed pstacks table

---
+++ src/archive/parse_pstacks.sh
@@ -0,0 +1,31 @@
+#!bin/bash
+
+# This script parses the output of pstacks into a table to determine the best value for minimum coverage.
+# Cyril Matthey-Doret
+# 11.04.2017
+
+module add R/latest
+
+pdir=/scratch/beegfs/monthly/cmatthey/data/pstacks
+echo 'min_cov,mean_loci,mean_alleles' > pstats.csv
+
+for d in $pdir/*;
+do
+    declare -i L=0 A=0 C=0
+    for f in $d/*.alleles.tsv.gz;
+    do
+        echo $L'_'$A
+        # Number of loci
+        L+=$(zcat $f | cut  -f3 | uniq | wc -l);
+        # Total number of alleles
+        A+=$(zcat $f | wc -l);
+        # Number of samples
+        C+=1
+    done;
+    FL=$(echo $L/$C | R --vanilla --quiet | sed -n '2s/.* //p')
+    FA=$(echo $A/$C | R --vanilla --quiet | sed -n '2s/.* //p')
+    echo ${d##*covmin-}','$FL','$FA >> pstats.csv
+done;
+
+module rm R/latest
+cp pstats.csv ../../reports/lab_book/
--- src/misc/parse_pstacks.sh
+++ src/misc/parse_pstacks.sh
@@ -1,31 +1,28 @@
 #!bin/bash
 
 # This script parses the output of pstacks into a table to determine the best value for minimum coverage.
+# Extracted metrics are: Mean coverage, st dev coverage, number of loci
 # Cyril Matthey-Doret
 # 11.04.2017
 
-module add R/latest
 
-pdir=/scratch/beegfs/monthly/cmatthey/data/pstacks
-echo 'min_cov,mean_loci,mean_alleles' > pstats.csv
 
-for d in $pdir/*;
+
+pdir=../../data/logs/pstacks
+echo "min_cov,nloci,mean_cov,sd_cov" > pstats.csv
+
+for f in $pdir/PST*;
 do
-    declare -i L=0 A=0 C=0
-    for f in $d/*.alleles.tsv.gz;
-    do
-        echo $L'_'$A
-        # Number of loci
-        L+=$(zcat $f | cut  -f3 | uniq | wc -l);
-        # Total number of alleles
-        A+=$(zcat $f | wc -l);
-        # Number of samples
-        C+=1
-    done;
-    FL=$(echo $L/$C | R --vanilla --quiet | sed -n '2s/.* //p')
-    FA=$(echo $A/$C | R --vanilla --quiet | sed -n '2s/.* //p')
-    echo ${d##*covmin-}','$FL','$FA >> pstats.csv
+    echo -n $(echo $f | sed 's/.*\([0-9]\).*/\1/')"," >> pstats.csv
+    awk 'BEGIN {FS = "\n"; OFS = ","; RS = ""; rds = 0; meancov = 0; sdcov = 0; nloc = 0; nstacks = 0; n = 0}
+    {
+    { match($5,/Analyzed [0-9]+/);AR = substr($5,RSTART+9,RLENGTH-9) }
+    if ( AR > 0 ) {
+        { match($11,/Wrote [0-9]+/);nloc += substr($6,RSTART+6,RLENGTH-6) }
+        { match($9,/is [0-9]+/);meancov += substr($9,RSTART+3,RLENGTH-3) }
+        { match($9,/Dev: [0-9]+/);sdcov += substr($9,RSTART+5,RLENGTH-5) }
+        {n+=1}
+    }}
+    END {print nloc/n,meancov/n,sdcov/n}' $f >> pstats.csv
 done;
-
-module rm R/latest
 cp pstats.csv ../../reports/lab_book/
