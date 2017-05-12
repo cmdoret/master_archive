9076260262a464c0cdd929e08870ec2417ba5ca3
cmdoret <cyril.mattheydoret@gmail.com>
2017-05-12 08:50:29 +0200

    waiting for mother's data to exclude haploid males. Started haplo_males.py script

--- src/ploidy/haplo_males.py
+++ src/ploidy/haplo_males.py
@@ -6,4 +6,14 @@
 import pandas as pd
 from sys import argv
 
-vcf_sum = pd.read_csv(argv[1])
+vcf_sum = pd.read_csv(argv[1], sep='\t')
+motherfam = pd.read_csv("../../data/mother_fam.txt",sep='\t')
+fam_sum = pd.merge(vcf_sum, motherfam, how='outer', left_on="INDV", right_on="Parent",
+         left_index=False, right_index=False, sort=False,
+         copy=True, indicator=False)
+print(fam_sum)
+# 1: read data/mother_fam.txt to extract families of mothers
+# 2: compute mean Fis of females in each family
+# 3: compute standard deviation of Fis in each family
+# 4: In each family: males with heterozygosity < meanF - NstdevF -> haploid
+# 5: output list of males with ploidy information
