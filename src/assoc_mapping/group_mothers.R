097d09d9ac1ec6e59647c13b7ebf094f916843a1
cmdoret <cyril.mattheydoret@gmail.com>
2017-08-23 16:30:09 +0200

    categorized mothers updated lab book with comments on family based association test

--- src/assoc_mapping/group_mothers.R
+++ src/assoc_mapping/group_mothers.R
@@ -3,9 +3,12 @@
 # Cyril Matthey-Doret
 # 30.07.2017
 
-library(dplyr)
+#==== LOADING DATA ====#
+library(dplyr);library(ggplot2)
 indv <- read.table("../../data/ploidy/thresholds/fixed", header=T)
+scenario <- 2  # Number of CSD loci considered
 
+#==== PROCESSING ====#
 diplo <- indv[indv$Generation=="F4" & indv$Ploidy=='D',]
 off_comp <- data.frame(table(diplo$Family, diplo$Sex))
 colnames(off_comp) <- c("Family", "Sex", "Count")
@@ -16,8 +19,27 @@ off_comp <- off_comp %>%
   filter(Sex=='M') %>%
   select(-Sex)
 
-hist(off_comp$prop_males,breaks=10, main="Male proportion in each family", xlab="Proportion of males", ylab="Number of families")
-print(off_comp)
+#=== CLUSTERING ===#
+# Expected number of categories is a function of the number of CSD loci
+ngroups <- 2^scenario-1
+# exp_prop <- 1/(2^scenario)
+
+# Using Pascal's triangle to get count of genotypes 
+# with n het loci where 0 < n <= scenario
+# Note: n < 0 since 0 het loci lead to male
+#x <- 1
+#for (i in 1:scenario) { x <- c(0, x) + c(x, 0)}
+
+# NUmber of centers for k-means clustering
+#centers <- sum(x[2:length(x)])  # First value is only homozygous loci
+
+km_output <- kmeans(off_comp$prop_males,centers = ngroups)
+off_comp$cluster <- km_output$cluster
+ggplot(data=off_comp, aes(x=prop_males, fill=as.factor(cluster))) + geom_histogram() +
+  ggtitle("Male proportion in each family") + xlab("Proportion of males") + 
+  ylab("Number of families") + geom_vline(data=data.frame(km_output$centers),aes(xintercept=km_output.centers), col='red', lty=2)
+abline(v=km_output$centers, col='red',lty=2)
+
 plot(off_comp$prop_males,off_comp$Count, xlab="Proportion of males", 
      ylab="Number of 2N offspring", main="Proportion of males versus total diploid offpsring")
 
