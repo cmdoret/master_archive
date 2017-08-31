c31dffa4a97c0380007340914a3b0def504a2613
cmdoret <cyril.mattheydoret@gmail.com>
2017-09-01 01:25:50 +0200

    documenting central vs terminal fusion script, improving visualization methods. Updated lab book with latest progress and ideas on association mapping and c. vs t. fusion

--- src/assoc_mapping/central_terminal_fusion.R
+++ src/assoc_mapping/central_terminal_fusion.R
@@ -8,43 +8,58 @@
 
 pack <- c("stringr","dplyr", "readr", "zoo", "ggplot2", "ggjoy", "reshape2")
 lapply(pack, require, character.only = TRUE)
-centrosize <- 1000000
 
 #==== LOADING DATA ====#
 
-# Path to folder containing STACKS populations output files
+# Path to genotype file. Encoded as 3 letters: E (het), O (hom) and M (missing)
 geno_path <- "../../data/assoc_mapping/grouped_geno_EOM.tsv"
-# Genomic output from populations
 gen <- read_tsv(geno_path, col_names = T)
 # Loading centromere list
 centro <- read_tsv("../../data/assoc_mapping/centro/centrolist.tsv")
 # Loading list of individuals
 indv <- read_tsv("../../data/individuals")
+# Keeping only individuals present in the genotype file and excluding mothers
 indv <- indv %>% 
   filter(Name %in% colnames(gen)[4:dim(gen)[2]]) %>%
   filter(Generation=='F4')
 #==== PROCESSING ===#
 
+# Excluding unordered contigs
 gen <- gen %>% 
   filter(str_detect(string=Chr, pattern="chr.*"))
-centro$start <- centro$pos - centrosize
-centro$end <- centro$pos + centrosize
+
+# Optional, only keeping large families for visualization
+big_fam <- indv %>%
+  group_by(Family) %>%
+  filter(n()>12)
+big_fam <- unique(big_fam$Family)
 
 #==== ANALYSIS ====#
 
+# Adding centromere positions to genotype file
 gen <- gen %>% 
   group_by(Chr) %>%
   merge(centro, by="Chr")
+# Using centromere position to compute distance from centromere
 gen <- gen %>%
   mutate(centroD=abs(gen$pos - gen$BP)) %>%
-  select(-start, -end, -pos, -val)
-
-# !!! CHECK THIS LINE !!!
+  select( -pos, -val)
+# Removing all sites which are missing for all offspring
 gen <- gen %>% filter_at(vars(indv$Name), any_vars(str_detect(string=.,pattern="[OE]")))
 
-slide_het <- function(seq,dist,w=50,step=1){
+#' slide_het
+#' Computes the proportion of heterozygous sites in a sliding window.
+#' @param seq A vector of characters, containing E (heterozygous) or O (homozygous) information.
+#' @param dist A vector of distance to centromere associated to the sites in seq.
+#' @param w The desired width of each window in number of sites.
+#' @param step The step between each window, in number of sites.
+#'
+#' @return a zoo series with the proportion of heterozygous sites in each window with associated 
+#' distance to centromere. The distance to centromere used is at the center of each window.
+slide_het <- function(seq,dist,w=30,step=1){
+  
   zoo_seq <- zoo(seq,order.by = dist)
-  win_het <- rollapply(zoo_seq,width = w, by=step, function(x){sum(x=='E')/sum(x %in% c('E','O'))})
+  win_het <- rollapply(zoo_seq,width = w, by=step, align='center', function(x){sum(x=='E')/sum(x %in% c('E','O'))})
   return(win_het)
 }
 
@@ -60,9 +75,10 @@ for(chrom in unique(gen$Chr)){
 }
 full_win <- melt(full_win, measure.vars = indv$Name, id.vars = c("Chr","centro_dist"), variable.name="Name",value.name="Het.")
 full_win <- merge(full_win, indv, by="Name")
+full_win$centro_dist <- as.numeric(full_win$centro_dist)
 
 genofull= data.frame()
-for(size in c(100000,3000000)){
+for(size in seq(10000,1000000,10000)){
   for ( chrom in unique(gen$Chr)){
     chr_start <- centro$pos[centro$Chr==chrom] - size
     chr_end <- centro$pos[centro$Chr==chrom] + size
@@ -85,9 +101,11 @@ for(size in c(100000,3000000)){
   print(size)
 }
 #==== VISUALISATION ====#
-
-ggplot(data=full_win, aes(x=centro_dist, y=Het., group=Name, col=Family)) + geom_line(stat='smooth', method='loess',alpha=0.3, se=F) + 
-  facet_grid(~Chr) + guides(col=FALSE)
+full_win <- full_win %>% 
+  group_by(Chr,Family) %>%
+  mutate(norm_het = (Het. - mean(Het.))/sd(Het.))
+ggplot(data=full_win[full_win$Family %in% big_fam,], aes(x=centro_dist, y=Het., col=Name,group=Name)) + 
+  geom_line(stat='smooth', method='loess',alpha=0.3, se=T) + guides(col=FALSE) + facet_grid(Family~Chr)
 
 # No correlation between number of loci in centromeric region and proportion of het.
 smoothScatter(genofull$Num.Loci, genofull$Het.)
@@ -95,10 +113,10 @@ famorder <- cen_chr %>%
   group_by(Family) %>%
   summarise(avg=mean(Het.))
 cen_chr$Family <- factor(cen_chr$Family, ordered = T, levels = famorder$Family[order(famorder$avg)])
-ggplot(data=cen_chr, aes(x=Het., y=Family)) + geom_joy()
+ggplot(data=genofull, aes(x=Het., y=Family)) + geom_joy()
 genofull <- genofull %>% 
   group_by(Chr, Family) %>%
   mutate(norm_het=(Het.-mean(Het., na.rm=T))/sd(Het., na.rm=T))
 
-ggplot(data=genofull, aes(x=centrosize, y=Het., col=Family, group=Name, weight=Num.Loci)) + geom_line() + 
-  facet_grid(.~Chr)+ guides(col=FALSE)
+ggplot(data=genofull[genofull$Family %in% big_fam,], aes(x=centrosize, y=Het.,col=as.factor(Name), weight=Num.Loci)) + stat_smooth(method='lm') + 
+  facet_grid(Family~Chr)+ guides(col=FALSE)
