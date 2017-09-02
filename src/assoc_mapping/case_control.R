77c2a6a58fa809762b7de1bb79af03d5b6188255
cmdoret <cyril.mattheydoret@gmail.com>
2017-09-02 12:24:17 +0200

    implemented fisher test for association mapping as well as correction for multiple testing and updated lab book results.

--- src/assoc_mapping/case_control.R
+++ src/assoc_mapping/case_control.R
@@ -25,8 +25,7 @@ sum_stat <- sum_stat[!is.na(sum_stat$cluster),]
 # Group SNP by mother category
 cat_stat <- sum_stat %>%
   group_by(Locus.ID, Chr, BP, cluster) %>%
-  summarise(N.Females=sum(N.Females), N.Males=sum(N.Males), 
-            N.Samples=sum(N.Samples), 
+  summarise(Nf=sum(N.Females), Nm=sum(N.Males), N=sum(N.Samples), 
             Prop.Hom=sum(Prop.Hom*N.Samples, na.rm=T)/sum(N.Samples, na.rm=T), 
             Prop.Hom.F=sum(Prop.Hom.F*N.Females, na.rm=T)/sum(N.Females, na.rm=T), 
             Prop.Hom.M=sum(Prop.Hom.M*N.Males, na.rm=T)/sum(N.Males, na.rm=T))
@@ -38,21 +37,32 @@ cat_stat <- sum_stat %>%
 #==== COMPUTE STATISTICS ====#
 # Abbreviations: M: Males, F: Females, T: Male+Female, 
 # o:homozygous, e:heterozygous, t:hom+het, E: Expected
+
+get_fisher <- function(df){
+  mat <- matrix(as.numeric(df[c(8:11)]), ncol=2)
+  f <- fisher.test(as.table(mat), alt="two.sided")
+  return(f$p.value)
+}
+
+
 odds_list <- cat_stat %>% 
-  rename(Ft = N.Females, Mt = N.Males, Tt = N.Samples) %>%
+  rename(Ft = Nf, Mt = Nm, Tt = N) %>%
   mutate(Fo = Ft * Prop.Hom.F, Mo = Mt * Prop.Hom.M, 
          Fe = Ft * (1-Prop.Hom.F), Me = Mt * (1-Prop.Hom.M),
          To = Tt * Prop.Hom, Te = Tt * (1-Prop.Hom)) %>%
-  select(-Prop.Hom, -Prop.Hom.F, -Prop.Hom.M) %>% 
-  mutate(EFo = Ft * To, EMo = Mt * To, EFe = Ft * Te, EMe = Mt * Te) %>%
-  mutate(chi2 = (Fo-EFo)^2/EFo + (Mo-EMo)^2/EMo + (Fe-EFe)^2/EFe + (Me-EMe)^2/EMe)
+  select(-Prop.Hom, -Prop.Hom.F, -Prop.Hom.M) %>%
+  mutate_at(funs(round(.,0)), .vars = c("Fo","Fe","Mo","Me"))
 
-odds_list$pval <- sapply(X = odds_list$chi2, function(x) pchisq(x, df=1, lower.tail=F, log.p=T))
-
+odds_list$fisher <- apply(odds_list, 1,  get_fisher)
+odds_list$fisher <- p.adjust(odds_list$fisher, method = "bonferroni")
+for(group in unique(odds_list$cluster)){
+  odds_list$fisher[odds_list$cluster==group] <- p.adjust(odds_list$fisher[odds_list$cluster==group], method = "BH")
+}
 #========= VISUALISE ========#
 odds_chrom <- odds_list[grep("chr.*",odds_list$Chr),]
-ggplot(data=odds_chrom, aes(x=BP, y=abs(pval))) + facet_grid(cluster~Chr, scales='free_x') + geom_point() + 
-  xlab("Genomic position") + ylab("-log2 p-value") + ggtitle("Case-control associaiton test for CSD")
+ggplot(data=odds_chrom, aes(x=BP, y=-log10(fisher))) + facet_grid(cluster~Chr, scales='free_x') + geom_point() + 
+  geom_hline(aes(yintercept=-log10(0.05))) + geom_hline(aes(yintercept=-log10(0.01)), lty=2, col='red') +
+  xlab("Genomic position") + ylab("-log10 p-value") + ggtitle("Case-control associaiton test for CSD")
 
 #======= WRITE OUTPUT =======#
 # Number of groups is (2^n)-1 where n is the number of CSD loci
