427ffdb549635fe6e16079edf3d540a87845f2b2
cmdoret <cyril.mattheydoret@gmail.com>
2017-11-10 00:30:37 +0100

    bug correction in pairwise blast script

--- src/homology/blast_chrom.sh
+++ src/homology/blast_chrom.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
 # This script blasts the chromosomes containing GWAS hits against one-another.
 # Cyril Matthey-Doret
 # 04.11.2017
+#BSUB -J chrom_blast
+#BSUB -q priority
+#BSUB -e ../../data/logs/chromoblast.err
+#BSUB -o ../../data/logs/chromoblast.out
+#BSUB -M 8000000
+#BSUB -R "rusage[mem=8000]"
+#BSUB -n 8
+#BSUB -R "span[ptile=8]"
 
 gwas='../../data/assoc_mapping/case_control/case_control_hits.tsv'
 blast_files="../../data/homology/blast_files/"
@@ -9,7 +18,6 @@ mkdir -p $blast_files
 
 module add Blast/ncbi-blast/2.6.0+;
 module add UHTS/Analysis/BEDTools/2.26.0
-
 # Extracting chromosomes containing sighificant GWAS hits
 chr_hits=$(cut -f2 $gwas | tail -n +2 | sort | uniq)
 
@@ -51,14 +59,23 @@ do
   file_id=$(basename $b_out)
   file_id=${file_id%.*}
   # Extracting query and subject chromosomes from filename
-  q=${file_id#*_}
-  s=${file_id%_*}
+  s=${file_id#*_}
+  q=${file_id%_*}
   echo "$b_out"
   awk -v ID=$file_id '{print $0,ID"_"NR}' $b_out > "$blast_files/b_tmp"
   mv "$blast_files/b_tmp" $b_out
   # Transform significant hits into intervals in bedfiles
-  awk '$11 < 1e-10 {print $1,$7,$8,$12}' $b_out > "$blast_files/${q}_${s}.bed"
-  awk '$11 < 1e-10 {print $2,$9,$10,$12}' $b_out > "$blast_files/${s}_${q}.bed"
+  awk 'BEGIN {
+         OFS="\t"}
+       $11 < 1e-10 {
+         print $1,$7,$8,$13,"0","+"}' $b_out > "$blast_files/${q}_${s}.bed"
+  awk 'BEGIN {
+         OFS="\t"}
+       $11 < 1e-10 {
+         if ($9 > $10)
+           print $2,$10,$9,$13,"0","-"
+         else
+           print $2,$9,$10,$13,"0","+"}' $b_out > "$blast_files/${s}_${q}.bed"
   echo "BLAST output parsed, unique alignment IDs added for $b_out !"
 done
 
@@ -93,24 +110,29 @@ done
 # IDs in inter_Q_a and inter_b_a)
 for Q in $chr_hits
 do
-  previous=""
-  for bed in "$blast_files/inter*$Q.bed"
+  prevID=""
+  for bed in $(find "$blast_files/" -name "inter*$Q.bed")
   do
-    if [ -z $first ]
+    echo $bed
+    if [ -z "$prevID" ]
     then
-      # First iteration: skip file and consider as previous
-      previous=$(cut -f4 $bed | sort)
+      echo "initialized"
+      # First iteration: prepare file IDs and consider as previous
+      cut -f4 $bed | sort | uniq > "temp_init"
+      prevID="temp_init"
     else
       echo "Extracting IDs of alignments present in all chromosomes."
       # Next iterations: intersection of ID between current and previous files
       # extracting IDs from bed files
-      currID=$(cut -f4 $bed | sort)
-      comm -12 <($prevID) <($currID) > temp_$bed
+      currID="$bed"
+      comm -12 <(echo $prevID | sort | uniq) \
+               <(cut -f4 $currID | sort | uniq) > "temp_$(basename $bed)"
+
       # Set current intersect with current file as previous
-      previous=$(cat "temp_$bed")
+      prevID="temp_$(basename bed)"
     fi
   done
-  mv "temp_$bed" "$blast_files/$Q_IDs.tsv"
+  mv "temp_$(basename $bed)" "$blast_files/$Q_IDs.tsv"
   rm temp_*
 done
 
