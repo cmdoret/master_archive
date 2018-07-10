8e3086a1b2c851e50c298881fe3669a0856837a1
cmdoret <cyril.mattheydoret@gmail.com>
2018-07-10 12:18:01 +0200

    fix issue where blacklist_haploloci.py breaks on integer-only sample names. Make blacklist_haploloci PEP8 compliant.

--- src/ploidy/blacklist_haploloci.py
+++ src/ploidy/blacklist_haploloci.py
@@ -13,14 +13,14 @@ parser = argparse.ArgumentParser(description="Uses EOM matrix made from the \
                                  loci that are heterozygous in haploid males \
                                  and stores them in a blacklist file.")
 
-parser.add_argument('EOM_in', type=str, help='Path to the EOM genotype matrix.')
+parser.add_argument('EOM_in', type=str, help='Path to the genotype matrix.')
 parser.add_argument('black_out', type=str,
                     help='Path to the output blacklist file.')
 parser.add_argument('ploid', type=str, help='Path to the table with ploidy \
                     information.')
 parser.add_argument('grouped_input', type=str,
-                                 help='If all families were grouped into a \
-                                 single populations run, set to "T", otherwise \
+                    help='If all families were grouped into a \
+                          single populations run, set to "T", otherwise \
                                  set to "F"')
 parser.add_argument('--haplo_het', type=float, default=0.5, help='Proportion \
                     (float between 0 and 1) of haploid males required to be \
@@ -32,31 +32,33 @@ EOM_prefix = 'grouped_' if args.grouped_input == "T" else "fam_"
 EOM_path = args.EOM_in + EOM_prefix + "geno_EOM.tsv"
 # Load genotype matrix and ploidy table
 EOM = pd.read_csv(EOM_path, sep='\t')
-ploid = pd.read_csv(args.ploid, sep='\t')
+ploid = pd.read_csv(args.ploid, sep='\t',
+                    converters={'Name': lambda x: str(x)})
 
 # Subset haploid individuals
-haplonames = ploid.loc[ploid.Ploidy=='H',"Name"]
-haplo_EOM = EOM.loc[:,haplonames]
+haplonames = ploid.loc[ploid.Ploidy == 'H', "Name"]
+haplo_EOM = EOM.loc[:, haplonames]
 
 
 # Suppresses warning when numpy divides by 0
 np.seterr(divide='ignore', invalid='ignore')
 
 dff = {}
-for t in ['O','M','E']:
+for t in ['O', 'M', 'E']:
     # Store arrays with number of E/O/M samples at each locus
-    dff[t] = (haplo_EOM==t).T.sum().astype(float)
+    dff[t] = (haplo_EOM == t).T.sum().astype(float)
 
 # Computing proportion of heterozygosity at each locus
 het = np.divide(dff['E'], (dff['O'] + dff['E']))
 het = het.fillna(0)
 
 # Extract loci above heterozygosity threshold
-het_idx = het.index[(het>args.haplo_het)]
-blacklist = EOM.iloc[het_idx,0].unique()
+het_idx = het.index[(het > args.haplo_het)]
+blacklist = EOM.iloc[het_idx, 0].unique()
 print("{0} loci are heterozygous in more than {1}% of haploids and \
 were removed from the analysis.".format(blacklist.shape[0],
-                                              args.haplo_het*100))
+                                        args.haplo_het*100))
 
 # Saving blacklisted loci to file
-pd.DataFrame(blacklist).to_csv(args.black_out, sep='\t', header=False, index=False)
+pd.DataFrame(blacklist).to_csv(args.black_out, sep='\t',
+                               header=False, index=False)
